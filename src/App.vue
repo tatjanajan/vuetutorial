9de8ce9cb98a8281a8038b734b167d7a4f1a4439
Tatjana Jankovic <tatjanazg@yahoo.com>
2019-08-01 12:19:40 +0200

    Kreiran todos objekt

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,27 @@ export default {
   name: 'app',
   components: {
   },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
+  }
 }
 </script>
 
