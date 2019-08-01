6114f98d6a164714cb1a06a62bd798df26e2285c
Tatjana Jankovic <tatjanazg@yahoo.com>
2019-08-01 18:29:33 +0200

    Kreirana komponent AddTodo s formom za dodavanje zadatka.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
   import Header from './components/layout/Header'
   import Todos from './components/Todos'
+  import AddTodo from './components/AddTodo'
 
   export default {
     name: 'app',
     components: {
       Header,
-      Todos
+      Todos,
+      AddTodo
     },
     data() {
       return {
@@ -54,5 +57,18 @@
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
