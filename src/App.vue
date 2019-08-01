8af8448a5c65f505fe146a0c59245076c6d295a8
Tatjana Jankovic <tatjanazg@yahoo.com>
2019-08-01 13:04:17 +0200

    Kreirana komponenta Todos

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,39 @@
 <template>
   <div id="app">
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
+  import Todos from './components/Todos'
 
-export default {
-  name: 'app',
-  components: {
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+  export default {
+    name: 'app',
+    components: {
+      Todos
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: "Todo One",
+            completed: false
+          },
+          {
+            id: 2,
+            title: "Todo Two",
+            completed: true
+          },
+          {
+            id: 3,
+            title: "Todo Three",
+            completed: false
+          }
+        ]
+      }
     }
   }
-}
 </script>
 
 <style>
